78aa1f77c62331528cab0d3a14de6a4eafd3e5d4
Emanuel-P16 <palazon.emanuel@gmail.com>
2022-11-01 15:03:13 -0300

    Instalacion socketio

--- backend/models/server.js
+++ backend/models/server.js
@@ -7,6 +7,8 @@ class Server {
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app)
+        this.io = require('socket.io')(this.server)
 
         this.paths = {
         }
@@ -15,6 +17,9 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        // Sockets
+        this.sockets();
     }
 
     async conectarDB() {
@@ -38,8 +43,16 @@ class Server {
         
     }
 
+    sockets(){
+        this.io.on('connection',socket=>{
+            console.log('cliente conectado' ,socket.id)
+            socket.on('disconnect',()=>{
+                console.log('Cliente desconectado', socket.id)
+            })
+        })
+    }
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
